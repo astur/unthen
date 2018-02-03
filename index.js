249bffb7b4b131c0598d8b263764fedf44071a2a
astur <astur@yandex.ru>
2018-02-03 21:26:06 +0300

    base code

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = () => {};
+module.exports = p =>
+    p instanceof Error ? Promise.resolve([p]) : typeof p.then === 'function' ? p.then(r => [null, r], e => [e]) : Promise.resolve([null, p]);
