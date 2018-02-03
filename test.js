444b0ecb000b45a95280c30aa9ff01b2ea743ebf
astur <astur@yandex.ru>
2018-02-03 21:37:09 +0300

    base tests

--- test.js
+++ test.js
@@ -1,7 +1,31 @@
 const test = require('ava');
 const unthen = require('.');
+const type = v => ({}).toString.call(v).slice(8, -1);
 
-test('unthen', t => {
-    t.true(true);
-    t.is(unthen, unthen);
+test('error', async t => {
+    const p = unthen(new Error());
+    t.is(type(p), 'Promise');
+    const r = await p;
+    t.deepEqual(r, [new Error()]);
+});
+
+test('not promise', async t => {
+    const p = unthen(1);
+    t.is(type(p), 'Promise');
+    const r = await p;
+    t.deepEqual(r, [null, 1]);
+});
+
+test('resolve', async t => {
+    const p = unthen(Promise.resolve(1));
+    t.is(type(p), 'Promise');
+    const r = await p;
+    t.deepEqual(r, [null, 1]);
+});
+
+test('reject', async t => {
+    const p = unthen(Promise.reject(new Error()));
+    t.is(type(p), 'Promise');
+    const r = await p;
+    t.deepEqual(r, [new Error()]);
 });
