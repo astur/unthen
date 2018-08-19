ec735d2c0f2a0cd709f370271976d34e4b7c7219
astur <astur@yandex.ru>
2018-08-19 13:09:11 +0300

    code linted

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-module.exports = p =>
-    p instanceof Error ? Promise.resolve([p]) : typeof p.then === 'function' ? p.then(r => [null, r], e => [e]) : Promise.resolve([null, p]);
+module.exports = p => p instanceof Error ? Promise.resolve([p]) : typeof p.then === 'function' ? p.then(r => [null, r], e => [e]) : Promise.resolve([null, p]);
